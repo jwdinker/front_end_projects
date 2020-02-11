1ff0f789edd9da302711b2ce952be3245c71317b
Jack <jwdinker.accounts@gmail.com>
2020-02-11 13:55:30 -0500

    useEventListener package was updated so sandbox was updated as well.

--- packages/sandbox/src/pages/use_event_listener/index.js
+++ packages/sandbox/src/pages/use_event_listener/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { Box, Row, Text, Centered, Fixed, Absolute, Relative } from '@jwdinker/styled-system';
-
+// @ts-check
 import useEventListener from '@jwdinker/use-event-listener';
 import { withCoreProviders } from '../../hocs';
 
