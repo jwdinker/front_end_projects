07224254a1b56eef5bf14c5325466df14ffb5272
Jack <jwdinker.accounts@gmail.com>
2020-03-19 18:08:39 -0400

    useBoundingClientRect - removed stale useMemo import.

--- packages/use_bounding_client_rect/src/index.ts
+++ packages/use_bounding_client_rect/src/index.ts
@@ -1,4 +1,4 @@
-import { useState, useMemo, useCallback } from 'react';
+import { useState, useCallback } from 'react';
 import useAnimationFrame from '@jwdinker/use-animation-frame';
 
 const INITIAL_STATE = {
