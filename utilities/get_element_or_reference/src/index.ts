5359558748c6ec13a41e21a29871964901f275a6
Jack <jwdinker.accounts@gmail.com>
2020-04-09 13:52:03 -0400

    getElementOrReference: initial commit.

---
+++ utilities/get_element_or_reference/src/index.ts
@@ -0,0 +1,15 @@
+import { RefObject } from 'react';
+
+export type ElementOrReference = RefObject<HTMLElement | undefined | null> | null | HTMLElement;
+
+export type GetElementReturn = HTMLElement | undefined | null;
+
+const getElementFromReference = (element: ElementOrReference): GetElementReturn => {
+  return element && 'current' in element && element.current instanceof HTMLElement
+    ? element.current
+    : element instanceof HTMLElement
+    ? element
+    : null;
+};
+
+export default getElementFromReference;
