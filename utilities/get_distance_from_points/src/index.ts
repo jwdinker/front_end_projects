91c5cbf990f7ea2c9693c1e2f4265e1fd4222470
Jack <jwdinker.accounts@gmail.com>
2020-04-10 14:02:35 -0400

    getDistanceFromPoints: initial commit.

---
+++ utilities/get_distance_from_points/src/index.ts
@@ -0,0 +1,13 @@
+export type Point = [number, number];
+export type Distance = [number, number, number];
+
+function getDistanceFromPoints(point: Point, center: Point): Distance {
+  const [x1, y1] = point;
+  const [x2, y2] = center;
+
+  const x = Math.sqrt((Math.max(x1, x2) - Math.min(x1, x2)) ** 2);
+  const y = Math.sqrt((Math.max(y1, y2) - Math.min(y1, y2)) ** 2);
+  return [x, y, x + y];
+}
+
+export default getDistanceFromPoints;
