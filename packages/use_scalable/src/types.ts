067c0e564a48d2949ee69c61ae0fbfc6d3e2cb20
Jack <jwdinker.accounts@gmail.com>
2020-04-10 14:09:46 -0400

    useScalable: added notes to UseScalableOptions.

--- packages/use_scalable/src/types.ts
+++ packages/use_scalable/src/types.ts
@@ -6,12 +6,39 @@ export type Distance = number[];
 export type Vector = number[];
 
 export interface UseScalableOptions {
+  /**
+   * The initial [x,y,z] scale values.
+   */
   initialScale?: Vector;
+
+  /**
+   * Minimum scale for the x,y,z axis.
+   */
   min?: Vector;
+
+  /**
+   * Maximum scale for the x,y,z axis.
+   */
   max?: Vector;
+
+  /**
+   * Initial height of the element used in conjunction with the top coordinate to calculate the center y coordinate.  Note: This is only needed if you are calculating from a single coordinate.
+   */
   height?: number;
+
+  /**
+   * Initial width of the element used in conjunction with the left coordinate to calculate the center x coordinate.  Note: This is only needed if you are calculating from a single coordinate.  Note: This is only needed if you are calculating from a single coordinate
+   */
   width?: number;
+
+  /**
+   * Initial top coordinate of the element used in conjunction with the height dimension to calculate the center y coordinate.  Note: This is only needed if you are calculating from a single coordinate.
+   */
   top?: number;
+
+  /**
+   * Initial left coordinate of the element used in conjunction with the width dimension to calculate the center x coordinate.  Note: This is only needed if you are calculating from a single coordinate.
+   */
   left?: number;
 }
 
