c6d5aed42843d7f4f502993ff901be14a03a7e36
Jack <jwdinker.accounts@gmail.com>
2020-08-23 11:36:02 -0400

    useDragListener: Removed error log and useMemo import.

--- packages/use_drag_listener/src/index.ts
+++ packages/use_drag_listener/src/index.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useMemo } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 import useEventListener, { UseEventListenerReturn } from '@jwdinker/use-event-listener';
 import useSSR from '@jwdinker/use-ssr';
 import { UseDragListenerProps, DragEvent, DragElement } from './types';
