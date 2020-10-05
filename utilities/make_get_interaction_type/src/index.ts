cf9ff608ffc10556cadcd49343b029c4d31bfed2
Jack <jwdinker.accounts@gmail.com>
2020-10-05 15:53:23 -0400

    Utilities:makeGetInteractionType: Added getInteractionType

--- utilities/make_get_interaction_type/src/index.ts
+++ utilities/make_get_interaction_type/src/index.ts
@@ -1,6 +1,7 @@
-export type InteractionType = 'mouse' | 'touch' | 'touches' | 'none';
-
-export type InteractionEvent = globalThis.TouchEvent | globalThis.MouseEvent;
+export type InteractionEvent =
+  | globalThis.TouchEvent
+  | globalThis.MouseEvent
+  | globalThis.WheelEvent;
 
 export interface InteractionTypes {
   MOUSE: InteractionType;
@@ -9,36 +10,73 @@ export interface InteractionTypes {
   NONE: InteractionType;
 }
 
-export const INTERACTION_TYPES: InteractionTypes = {
+export const INTERACTION_TYPES = {
   MOUSE: 'mouse',
   TOUCH: 'touch',
   TOUCHES: 'touches',
+  WHEEL: 'wheel',
   NONE: 'none',
-};
+  DORMANT: 'dormant',
+  IDLE: 'idle',
+} as const;
+
+export type InteractionType = typeof INTERACTION_TYPES[keyof typeof INTERACTION_TYPES];
 
 function areTouchEventsSupported(): boolean {
   return typeof window !== 'undefined' && window.ontouchstart === null;
 }
-const { MOUSE, TOUCH, TOUCHES, NONE } = INTERACTION_TYPES;
+const { MOUSE, TOUCH, TOUCHES, NONE, WHEEL } = INTERACTION_TYPES;
+
+export function getInteractionType(event: InteractionEvent): InteractionType {
+  const touches =
+    !!event && areTouchEventsSupported() && event instanceof TouchEvent ? event.touches : null;
+
+  const isSingleTouch = touches && touches.length === 1;
+
+  if (isSingleTouch) {
+    return TOUCH;
+  }
+
+  const isMultiTouch = touches && touches.length > 1;
+
+  if (isMultiTouch) {
+    return TOUCHES;
+  }
+
+  const isMouse = !!event && event instanceof MouseEvent;
+  if (isMouse) {
+    return MOUSE;
+  }
+
+  const isWheel = !!event && event instanceof WheelEvent;
+  if (isWheel) {
+    return WHEEL;
+  }
+  return NONE;
+}
 
 function makeGetInteractionType(mouse = false, touch = 2) {
   return (event: InteractionEvent): InteractionType => {
     const touches =
       !!event && areTouchEventsSupported() && event instanceof TouchEvent ? event.touches : null;
     const isSingleTouch = touch === 1 && touches && touches.length === 1;
-    const isMultiTouch = touch > 1 && touches && touches.length > 1;
-    const isMouse = mouse && !!event && event instanceof MouseEvent;
 
     if (isSingleTouch) {
       return TOUCH;
     }
 
+    const isMultiTouch = touch > 1 && touches && touches.length > 1;
     if (isMultiTouch) {
       return TOUCHES;
     }
-
+    const isMouse = mouse && !!event && event instanceof MouseEvent;
     if (isMouse) {
       return MOUSE;
+    }
+
+    const isWheel = !!event && event instanceof WheelEvent;
+    if (isWheel) {
+      return WHEEL;
     }
     return NONE;
   };
