0526d231d971ac53bbd9da317203c82763ca1b08
Jack <jwdinker.accounts@gmail.com>
2020-10-05 16:29:58 -0400

    useInteractivity: Removed

--- packages/use_interactivity/src/helpers/calculate_velocity.js
+++
@@ -1,9 +0,0 @@
-function calculateVelocity(distance, startTime, endTime) {
-  const denominator = endTime - startTime;
-  if (Number.isNaN(denominator) || denominator === 0) {
-    return 0;
-  }
-  return Math.abs(distance / denominator);
-}
-
-export default calculateVelocity;
--- packages/use_interactivity/src/helpers/index.js
+++
@@ -1,3 +0,0 @@
-export { default as calculateVelocity } from './calculate_velocity';
-export { default as getDirection } from './get_direction';
-export { default as getPhase } from './get_phase';
