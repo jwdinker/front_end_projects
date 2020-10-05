52ff22767ef49be4f2a31223e01057cc5e783fef
Jack <jwdinker.accounts@gmail.com>
2020-10-05 15:55:22 -0400

    Utilities: Added Previous Result to upTo Callback

--- utilities/up_to/src/index.ts
+++ utilities/up_to/src/index.ts
@@ -2,7 +2,7 @@ function upTo<T>(start: number, end: number, callback: Function): T[] {
   let index = start;
   const result: T[] = [];
   while (index <= end) {
-    result.push(callback(index));
+    result.push(callback(index, result));
     index += 1;
   }
   return result;
