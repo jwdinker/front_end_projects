2407ee37cea1b3c0c8d0054dd8518334dba4b316
Jack <jwdinker.accounts@gmail.com>
2020-10-05 15:29:47 -0400

    useForceUpdate: Initial Commit.

---
+++ packages/use_force_update/src/index.ts
@@ -0,0 +1,24 @@
+import * as React from 'react';
+
+const { useState, useCallback, useRef, useEffect } = React;
+
+export type ForceUpdate = () => void;
+export type NumberOfForcedUpdates = number;
+export type ForceUpdateReturn = [ForceUpdate, boolean];
+
+function useForceUpdate(): ForceUpdateReturn {
+  const [count, setCount] = useState(0);
+  const previousCount = useRef(0);
+
+  const force = useCallback((): void => {
+    setCount((previous) => previous + 1);
+  }, []);
+
+  useEffect(() => {
+    previousCount.current = count;
+  }, [count]);
+
+  return [force, previousCount.current !== count];
+}
+
+export default useForceUpdate;
