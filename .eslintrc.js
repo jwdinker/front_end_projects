41203ed13495215ab999f3bad54915a14ae284f7
Jack <jwdinker.accounts@gmail.com>
2020-10-06 18:24:28 -0400

    Updated Config Files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,17 @@
 module.exports = {
-  extends: ['airbnb', 'airbnb/hooks', 'prettier', 'prettier/@typescript-eslint', 'prettier/react'],
-  plugins: ['@typescript-eslint'],
+  extends: [
+    // imports rules and plugins.
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    //'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/typescript',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react',
+  ],
+  plugins: ['@typescript-eslint'], //defines whats available.
   parser: 'babel-eslint',
   env: {
     browser: true,
@@ -11,18 +22,24 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module', //Prevents import and export linting errors
+    //project: 'tsconfig.json',
   },
   rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
     'react-hooks/exhaustive-deps': 'warn',
     'react-hooks/rules-of-hooks': 'error',
     'react/prop-types': 0,
     'spaced-comment': ['error', 'always', { markers: ['/', '!'] }],
     'no-underscore-dangle': 'off',
     'no-nested-ternary': 'off',
+    'react/jsx-filename-extension': 0,
+    'import/prefer-default-export': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
   },
   overrides: [
     {
-      files: ['**/*.ts'],
+      files: ['**/*.ts', '**/*.tsx'],
       parser: '@typescript-eslint/parser',
     },
   ],
