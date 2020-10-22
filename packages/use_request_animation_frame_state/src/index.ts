14ad323995769455e35cadd987a44b23139cc460
Jack <jwdinker.accounts@gmail.com>
2020-10-22 16:24:32 -0400

    useRequestAnimationFrameState: Added to Improve useTether Performance.

---
+++ packages/use_request_animation_frame_state/src/index.ts
@@ -0,0 +1,29 @@
+import { useRef, useCallback, useState, useEffect, Dispatch, SetStateAction } from 'react';
+
+function useRequestAnimationFrameState<S>(
+  initialState: S | (() => S)
+): [S, Dispatch<SetStateAction<S>>] {
+  const id = useRef<number | null>(null);
+  const [state, setState] = useState(initialState);
+
+  const setRequestAnimationFrameState = useCallback((nextState: S | ((prevState: S) => S)) => {
+    if (id.current) {
+      window.cancelAnimationFrame(id.current);
+    }
+    id.current = window.requestAnimationFrame(() => {
+      setState(nextState);
+    });
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      if (id.current) {
+        cancelAnimationFrame(id.current);
+      }
+    };
+  }, []);
+
+  return [state, setRequestAnimationFrameState];
+}
+
+export default useRequestAnimationFrameState;
