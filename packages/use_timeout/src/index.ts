77afbe0eb1b4044c389699fafa3795dae4877c5f
Jack <jwdinker.accounts@gmail.com>
2020-11-13 17:26:24 -0500

    useTimeout: Incorporated Alternative window.requestAnimationFrame Loop.

--- packages/use_timeout/src/index.ts
+++ packages/use_timeout/src/index.ts
@@ -1,36 +1,66 @@
 import { useEffect, useRef, useCallback } from 'react';
-import { UseTimeoutCallback, UseTimeoutReturn } from './types';
+import { UseTimeoutCallback, UseTimeoutReturn, onTimeout } from './types';
+
+const hasRequestAnimationFrame =
+  typeof window !== 'undefined' && typeof window.requestAnimationFrame === 'function';
+
+const getTimer = (fn: onTimeout, milliseconds: number) => {
+  if (hasRequestAnimationFrame) {
+    return window.requestAnimationFrame(fn);
+  }
+  return setTimeout(fn, milliseconds);
+};
 
 /**
  *
  * @param callback The callback that will be executed at the end of the timeout.
  * @param milliseconds The wait duration before the callback is executed.
  */
-function useTimeout(
-  callback: UseTimeoutCallback = (): void => {},
-  milliseconds = 0
-): UseTimeoutReturn {
-  const timeout = useRef<number>();
-  const saved = useRef<UseTimeoutCallback | undefined>();
+function useTimeout(callback: UseTimeoutCallback, milliseconds = 0): UseTimeoutReturn {
+  const timer = useRef<number>();
+  const _callback = useRef<UseTimeoutCallback | undefined>();
+  const startTime = useRef(0);
+
+  useEffect(() => {
+    _callback.current = callback;
+  }, [callback]);
 
   const clear = useCallback(() => {
-    if (timeout.current) {
-      clearTimeout(timeout.current);
+    if (timer.current) {
+      if (hasRequestAnimationFrame) {
+        startTime.current = 0;
+        window.cancelAnimationFrame(timer.current);
+      } else {
+        clearTimeout(timer.current);
+      }
+      timer.current = undefined;
     }
   }, []);
 
-  useEffect(() => {
-    saved.current = callback;
-  }, [callback]);
+  const onExpiration = useCallback(() => {
+    if (_callback.current) {
+      if (hasRequestAnimationFrame) {
+        const time = Date.now();
+        const duration = time - startTime.current;
+        const isWaitThresholdMet = duration >= milliseconds;
+
+        if (isWaitThresholdMet) {
+          _callback.current();
+          clear();
+        } else {
+          timer.current = getTimer(onExpiration, milliseconds);
+        }
+      } else {
+        _callback.current();
+        clear();
+      }
+    }
+  }, [milliseconds, clear]);
 
   const start = useCallback(() => {
-    timeout.current = window.setTimeout(() => {
-      if (saved.current) {
-        saved.current();
-      }
-      clear();
-    }, milliseconds);
-  }, [clear, milliseconds]);
+    startTime.current = Date.now();
+    timer.current = getTimer(onExpiration, milliseconds);
+  }, [milliseconds, onExpiration]);
 
   useEffect(() => {
     return clear;
--- packages/use_timeout/src/types.ts
+++ packages/use_timeout/src/types.ts
@@ -5,3 +5,5 @@ export type StartTimeout = () => void;
 export type ClearTimeout = () => void;
 
 export type UseTimeoutReturn = [StartTimeout, ClearTimeout];
+
+export type onTimeout = (id: number) => void;
