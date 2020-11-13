99f6d3a1f91c9db6551b62ef75d9fd9986ad20f2
Jack <jwdinker.accounts@gmail.com>
2020-11-13 17:28:19 -0500

    useElementsReferencesChange: Added Comments and Fixed Mount bug.
    React fast refresh holds on to previous ref values so the comparisons to current refs were causing stale values because onReference wasn't being called.

--- packages/use_element_references_change/src/types.ts
+++ packages/use_element_references_change/src/types.ts
@@ -1,15 +1,26 @@
-export type ElementReference = HTMLElement | null | undefined;
-
 export type ReferenceIndex = number;
-
-export type ChangedElementReferences = [ReferenceIndex, ElementReference];
 
 export type ReferencedElement = [ReferenceIndex, HTMLElement];
 
 export type ReferenceCallback = (referencedElements: ReferencedElement[]) => void;
 
 export interface UseElementReferencesOptions {
+  /**
+   * A callback invoked anytime a reference changes from undefined/null to
+   * HTMLElement. An array of arrays containing the index and corresponding
+   * HTMLElement is passed to the callback.
+   */
   onReference?: ReferenceCallback;
-  onRemove?: ReferenceCallback;
+
+  /**
+   * A callback invoked anytime a reference changes from HTMLElement to null excluding the unmount. An
+   * array of arrays containing the index and previously referenced HTMLElement
+   * is passed to the callback.
+   */
+  onDereference?: ReferenceCallback;
+
+  /**
+   * A callback invoked when the hook executes the final cleanup.
+   */
   onUnmount?(): void;
 }
