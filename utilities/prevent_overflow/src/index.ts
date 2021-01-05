f41a5c3b02c6254d6a01bf83769f22fe872b9dfe
Jack <jwdinker.accounts@gmail.com>
2021-01-05 17:36:16 -0500

    Utilities:preventOverflow: made padding per side optional.

--- utilities/prevent_overflow/src/index.ts
+++ utilities/prevent_overflow/src/index.ts
@@ -6,10 +6,10 @@ export interface AbbreviatedRectangle {
 }
 
 export interface Padding {
-  top: number;
-  bottom: number;
-  left: number;
-  right: number;
+  top?: number;
+  bottom?: number;
+  left?: number;
+  right?: number;
 }
 
 export type Side = 'top' | 'bottom' | 'left' | 'right';
@@ -35,15 +35,9 @@ export type PreventOverflowReturn = [PreventOverflowValues, PreventedSides];
 
 const SIDES = ['top', 'bottom', 'left', 'right'] as const;
 
-export const DEFAULT_PADDING: Padding = {
-  top: 0,
-  left: 0,
-  right: 0,
-  bottom: 0,
-};
-
 function preventOverflow(props: PreventOverflowProps): PreventOverflowReturn {
-  const { element, boundaries, padding = DEFAULT_PADDING, allow = [] } = props;
+  const { element, boundaries, padding = {}, allow = [] } = props;
+  const { top = 0, left = 0, right = 0, bottom = 0 } = padding;
 
   const allowed = SIDES.reduce((accumulator, side) => {
     accumulator[side] = allow.indexOf(side) > -1;
@@ -56,23 +50,23 @@ function preventOverflow(props: PreventOverflowProps): PreventOverflowReturn {
   const bottomBoundary = boundaries.top + boundaries.height;
   const rightBoundary = boundaries.left + boundaries.width;
 
-  if (!allowed.top && element.top <= boundaries.top + padding.top) {
-    values.top = boundaries.top + padding.top;
+  if (!allowed.top && element.top <= boundaries.top + top) {
+    values.top = boundaries.top + top;
     prevented.push('top');
   }
 
-  if (!allowed.bottom && element.top >= bottomBoundary - element.height + padding.bottom) {
-    values.top = bottomBoundary - element.height + padding.bottom;
+  if (!allowed.bottom && element.top >= bottomBoundary - element.height + bottom) {
+    values.top = bottomBoundary - element.height + bottom;
     prevented.push('bottom');
   }
 
-  if (!allowed.left && element.left <= boundaries.left + padding.left) {
-    values.left = boundaries.left + padding.left;
+  if (!allowed.left && element.left <= boundaries.left + left) {
+    values.left = boundaries.left + left;
     prevented.push('left');
   }
 
-  if (!allowed.right && element.left >= rightBoundary - element.width + padding.right) {
-    values.left = rightBoundary - element.width + padding.right;
+  if (!allowed.right && element.left >= rightBoundary - element.width + right) {
+    values.left = rightBoundary - element.width + right;
     prevented.push('right');
   }
 
