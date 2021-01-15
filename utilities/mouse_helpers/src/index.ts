facf7ec5113e5da71ff3dc99586199beb1f3da95
Jack <jwdinker.accounts@gmail.com>
2021-01-15 15:45:18 -0500

    Utilities: Removed phase helpers and mouse helpers

--- utilities/mouse_helpers/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { MouseCoordinates } from './types';
-
-export { MouseCoordinates } from './types';
-
-export function getMouseCoordinates(event: MouseEvent, coordinateType = 'page'): MouseCoordinates {
-  const x = `${coordinateType}X`;
-  const y = `${coordinateType}Y`;
-  return [event[x], event[y]];
-}
