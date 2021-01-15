55335150583c02111959d77830129f6aa0f7f64b
Jack <jwdinker.accounts@gmail.com>
2021-01-14 19:12:58 -0500

    getHTMLElementFromReference: added SSR check.
    This will prevent errors on server side rendering when checking the instance of HTMLElement.

--- utilities/get_html_element_from_reference/src/index.ts
+++ utilities/get_html_element_from_reference/src/index.ts
@@ -1,8 +1,9 @@
 export type HTMLElementReference = React.RefObject<HTMLElement | undefined | null>;
 
-export type GetHTMLElementReferenceReturn = HTMLElement | undefined | null;
-
 function getHTMLElementReference(element: HTMLElementReference): HTMLElement | null | undefined {
+  if (typeof window === 'undefined') {
+    return undefined;
+  }
   if (element && 'current' in element) {
     if (element.current instanceof HTMLElement) {
       return element.current;
