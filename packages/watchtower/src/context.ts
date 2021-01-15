afccfc4fde51c2a0550fad84f41112992964cc22
Jack <jwdinker.accounts@gmail.com>
2021-01-15 14:37:24 -0500

    Watchtower: Removed. useContext not good for rapid updates.

--- packages/watchtower/src/context.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react';
-import { INITIAL_CONTEXT } from './constants';
-
-const Context = createContext(INITIAL_CONTEXT);
-
-export default Context;
--- packages/watchtower/src/index.js
+++
@@ -1,3 +0,0 @@
-export { default as WatchTower } from './watch_tower';
-export { default as useWatchTowerContext } from './use_watch_tower_context';
-export { default as useBeacon } from './use_beacon';
